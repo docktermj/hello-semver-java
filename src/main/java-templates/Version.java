62c7ecbe1eca80a7edc18eeed6fed4c0b65f40f9
Michael Dockter <michael@dockter.com>
2018-09-14 12:48:35 -0400

    issue-1 Working version injected into Java source

---
+++ src/main/java-templates/Version.java
@@ -0,0 +1,14 @@
+package com.dockter.semver.java;
+
+public final class Version {
+
+	private static final String VERSION = "${project.version}";
+	private static final String GROUPID = "${project.groupId}";
+	private static final String ARTIFACTID = "${project.artifactId}";
+	private static final String REVISION = "${buildNumber}";
+
+	public static String getVersion() {
+		return VERSION;
+	}
+
+}
---
+++ src/main/java/com/dockter/semver/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.dockter.semver.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World! version 0.0.0");
+		System.out.println("Code version: 0.0.0");
+		System.out.println("Git  version:" + Version.getVersion());
+	}
+}
