b53fcd51e65a50ea832d9e26196b2041df1ed72e
Michael Dockter <michael@dockter.com>
2018-09-17 16:11:32 -0400

    issue-1 Added Dockerfile and examplar git commands

--- src/main/java/com/dockter/semver/java/HelloWorld.java
+++ src/main/java/com/dockter/semver/java/HelloWorld.java
@@ -5,7 +5,7 @@ package com.dockter.semver.java;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World! version 0.0.0");
+		System.out.println("Hello World!");
 		System.out.println("Code version: 0.0.4");
 		System.out.println("Git  version: " + BuildInfo.getVersion());
 	}
