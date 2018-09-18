3545fcfe3d430380348a6c64bd0af1e40051bbb7
Michael Dockter <michael@dockter.com>
2018-09-18 14:13:41 -0400

    issue-1 Add branch to BuildInfo

--- src/main/java/com/dockter/semver/java/HelloWorld.java
+++ src/main/java/com/dockter/semver/java/HelloWorld.java
@@ -5,8 +5,12 @@ package com.dockter.semver.java;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
+		System.out.println("");
 		System.out.println("Hello World!");
-		System.out.println("Code version: 0.0.7");
+		System.out.println("Code version: 0.0.8");
 		System.out.println("Git  version: " + BuildInfo.getVersion());
+		System.out.println("Git      SHA: " + BuildInfo.getGitSHA());
+		System.out.println("Build   date: " + BuildInfo.getTimestamp());
+		System.out.println("");
 	}
 }
